190ee35b1c8b0785ca801f7dfe99791b26394cdd
Vedran Blazenka <blazenka.vedran@gmail.com>
2022-09-07 20:16:26 +0200

    feat(elm): solve annalyns-infiltration

---
+++ elm/annalyns-infiltration/src/AnnalynsInfiltration.elm
@@ -0,0 +1,30 @@
+module AnnalynsInfiltration exposing (canFastAttack, canFreePrisoner, canSignalPrisoner, canSpy, stealthAttackDamage)
+
+
+canFastAttack : Bool -> Bool
+canFastAttack knightIsAwake =
+    not knightIsAwake
+
+
+canSpy : Bool -> Bool -> Bool -> Bool
+canSpy knightIsAwake archerIsAwake prisonerIsAwake =
+    knightIsAwake || archerIsAwake || prisonerIsAwake
+
+
+canSignalPrisoner : Bool -> Bool -> Bool
+canSignalPrisoner archerIsAwake prisonerIsAwake =
+    not archerIsAwake && prisonerIsAwake
+
+
+canFreePrisoner : Bool -> Bool -> Bool -> Bool -> Bool
+canFreePrisoner knightIsAwake archerIsAwake prisonerIsAwake petDogIsPresent =
+    petDogIsPresent && not archerIsAwake || not petDogIsPresent && prisonerIsAwake && not knightIsAwake && not archerIsAwake
+
+
+stealthAttackDamage : Bool -> Int
+stealthAttackDamage annalynIsDetected =
+    if annalynIsDetected then
+        7
+
+    else
+        12
