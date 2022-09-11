29797ac6ed7eecbaffb55a3964c8f13a1466fa8a
Vedran Blazenka <blazenka.vedran@gmail.com>
2022-09-11 16:52:31 +0200

    feat(elm): solve tisbury-treasure-hunt

--- elm/tisbury-treasure-hunt/src/TisburyTreasureHunt.elm
+++ elm/tisbury-treasure-hunt/src/TisburyTreasureHunt.elm
@@ -26,21 +26,36 @@ placeLocationToTreasureLocation placeLocation =
 
 treasureLocationMatchesPlaceLocation : PlaceLocation -> TreasureLocation -> Bool
 treasureLocationMatchesPlaceLocation placeLocation treasureLocation =
-    let
-        ( plc, pli ) =
-            placeLocation
-
-        ( tli, tlc ) =
-            treasureLocation
-    in
-    plc == tlc && pli == tli
+    placeLocation
+        |> placeLocationToTreasureLocation
+        |> (==) treasureLocation
 
 
 countPlaceTreasures : Place -> List Treasure -> Int
-countPlaceTreasures place treasures =
-    Debug.todo "implement this function"
+countPlaceTreasures ( _, placeLocation ) treasures =
+    treasures
+        |> List.map (\( _, treasureLocation ) -> treasureLocation)
+        |> List.filter (treasureLocationMatchesPlaceLocation placeLocation)
+        |> List.length
 
 
-specialCaseSwapPossible : ( String, TreasureLocation ) -> ( String, PlaceLocation ) -> ( String, TreasureLocation ) -> Bool
+specialCaseSwapPossible : Treasure -> Place -> Treasure -> Bool
 specialCaseSwapPossible ( foundTreasure, _ ) ( place, _ ) ( desiredTreasure, _ ) =
-    Debug.todo "implement this function"
+    case ( foundTreasure, place, desiredTreasure ) of
+        ( "Brass Spyglass", "Abandoned Lighthouse", _ ) ->
+            True
+
+        ( "Amethyst Octopus", "Stormy Breakwater", "Crystal Crab" ) ->
+            True
+
+        ( "Amethyst Octopus", "Stormy Breakwater", "Glass Starfish" ) ->
+            True
+
+        ( "Vintage Pirate Hat", "Harbor Managers Office", "Model Ship in Large Bottle" ) ->
+            True
+
+        ( "Vintage Pirate Hat", "Harbor Managers Office", "Antique Glass Fishnet Float" ) ->
+            True
+
+        _ ->
+            False
